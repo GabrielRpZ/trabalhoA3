6d6a04f65b1d84fa076fc91c295bf09948ea5cba
GabrielRpZ <gabrieljardim049@gmail.com>
2024-10-30 12:14:09 -0300

    Update Livro.java

--- src/Livro.java
+++ src/Livro.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Livro {
@@ -17,7 +18,7 @@ public class Livro {
     }
 
     //menu Livro
-    public void menuLivro(){
+    public  void menuLivro(){
         Scanner input = new Scanner(System.in);
         Estoque estoque = new Estoque();
 
@@ -38,8 +39,22 @@ public class Livro {
                     cadastrarNovoLivro();
                     break;
                 case 2:
-
+					
                     break;
+                
+             
+  					case 3:
+   						 System.out.print("Digite o ISBN do livro que deseja exibir: ");
+    					 String isbn = input.nextLine();
+    					 exibirLivroPorISBN(isbn);
+    				break;
+
+	
+				
+                
+                case 4:
+					MostrarLivros();
+					break;
             
                 default:
                     break;
@@ -47,7 +62,7 @@ public class Livro {
             
         }while(opcaoLivro != 5);
     }
-
+	private static ArrayList<Livro> listaLivros = new ArrayList<Livro>();
     public void cadastrarNovoLivro(){
         Scanner input = new Scanner(System.in);
         Estoque estoque = new Estoque();
@@ -59,15 +74,67 @@ public class Livro {
         String autor = input.nextLine();
 
         System.out.print("Digite o ISBN do livro: ");
-        String isbn = input.nextLine();
+        isbn = input.nextLine();
+		    for (Livro livro : listaLivros) {
+        if (livro.getIsbn().equals(isbn)) {
+            System.out.println("Erro: Um livro com este ISBN já está cadastrado!");
+        	return;
+        }
+
+    }//verifica se o ISBN ja foi cadastrado
+		
+
 
         System.out.print("Digite a quantidade de exemplares no estoque: ");
         int quantidadeEstoque = input.nextInt();
 
         Livro novoLivro = new Livro(titulo, autor, isbn, quantidadeEstoque);
         estoque.adicionarLivro(novoLivro);
+		listaLivros.add(novoLivro);
         System.out.println("Livro adicionado ao estoque com sucesso!");
+  		
+      
+      
     }
+
+
+  //MOSTRAR LIVROS
+		public static void MostrarLivros(){
+		System.out.print("Livros Cadastrados: ");
+		System.out.println(listaLivros.size());
+
+ 		for(int i=0; i < listaLivros.size(); i++){
+		System.out.println("Titulo livro: " + listaLivros.get(i).titulo + "\nAutor do Livro: " + listaLivros.get(i).autor + "\nIsbn: " + listaLivros.get(i).isbn + "\nQuantidade em estoque: " + listaLivros.get(i).quantidadeEstoque);
+		System.out.println("---------------");
+		
+
+        }
+		System.out.println("Voltando ao menu...");
+
+
+}
+		
+
+ 	public static void exibirLivroPorISBN(String isbn) {
+    boolean livroEncontrado = false;
+    
+    for (Livro livro : listaLivros) {
+        if (livro.getIsbn().equals(isbn)) {
+            System.out.println("Título: " + livro.getTitulo());
+            System.out.println("Autor: " + livro.getAutor());
+            System.out.println("ISBN: " + livro.getIsbn());
+            System.out.println("Quantidade em estoque: " + livro.getQuantidadeEstoque());
+			System.out.println("Voltando ao menu...");
+            livroEncontrado = true;
+            break;
+        }
+    }
+    
+    if (!livroEncontrado) {
+        System.out.println("Livro com ISBN " + isbn + " não encontrado.");
+		System.out.println("Voltando ao menu...");
+    }
+}
 
     //Getters e Setters
     public String getTitulo(){
@@ -101,6 +168,8 @@ public class Livro {
             this.quantidadeEstoque -= quantidade;
         }else{
             System.out.println("Estoque insuficiente!");
+
+
         }
     }
 }
